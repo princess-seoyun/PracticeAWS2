f944f05759f6d0a58f83de4716bf9ef6d937e45e
seoyunZZang <p815000@naver.com>
2024-03-20 03:02:49 +0900

    Main 바꿔서 테스트 진행

--- src/main/java/com/example/diary_back/controller/MainController.java
+++ src/main/java/com/example/diary_back/controller/MainController.java
@@ -1,14 +1,19 @@
 package com.example.diary_back.controller;
 
+import com.example.diary_back.dto.SaveDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/diary")
 public class MainController {
 
-    @GetMapping("/")
+    @GetMapping("/main")
     public String mainP() {
 
-        return "main";
+        String main = "메인 페이지";
+
+        return main;
     }
 }
